58f722bdf47d3265ec7eb94690a76c188a2bf4ce
Jeff Kolesky <jeff@kolesky.com>
2013-01-02 18:13:08 -0800

    Reverses isMultiple arguments to allow for currying

--- project_euler/001_multiples_of_3_and_5.hs
+++ project_euler/001_multiples_of_3_and_5.hs
@@ -5,19 +5,19 @@
 
 import System
 
--- Returns true if x is a multiple of y
-isMultiple :: Integral a => a -> a -> Bool
-isMultiple x 0 = False
-isMultiple x y = x `mod` y == 0
+-- Returns true if y is a multiple of x
+isMultipleOf :: Integral a => a -> a -> Bool
+isMultipleOf 0 _ = False
+isMultipleOf x y = y `mod` x == 0
 
-isMultiple' :: Integral a => a -> Bool
-isMultiple' x = isMultiple x 3 || isMultiple x 5
+isMultiple :: Integral a => a -> Bool
+isMultiple x = isMultipleOf 3 x || isMultipleOf 5 x
 
 sumMultiples :: Integral a => [a] -> a
-sumMultiples xs = sum (filter isMultiple' xs)
+sumMultiples xs = sum (filter isMultiple xs)
 
 main = do
     (n:ns) <- getArgs
     let max = read n
-    putStrLn ("The sum of all multiples of 3 or 5 below " ++ n ++ " is " ++
-        (show $ sumMultiples [1..(max-1)]))
+    putStrLn $ "The sum of all multiples of 3 or 5 below " ++ n ++ " is " ++
+        (show $ sumMultiples [1..(max-1)])
