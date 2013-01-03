cdb682370c18bf2213062cf489d87022633e1f4c
Jeff Kolesky <jeff@kolesky.com>
2013-01-02 18:28:24 -0800

    Uses applicative functors
    It is not totally clear to me that this is easier to read, but perhaps
    if I were a Haskell expert, then I would be used to this syntax.  But
    really `isMultipleOf3 x || isMultipleOf5 x` reads really well to me.

--- project_euler/001_multiples_of_3_and_5.hs
+++ project_euler/001_multiples_of_3_and_5.hs
@@ -4,19 +4,22 @@
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 
 import System
+import Control.Applicative
 
 -- Returns true if y is a multiple of x
 isMultipleOf :: Integral a => a -> a -> Bool
 isMultipleOf 0 _ = False
 isMultipleOf x y = y `mod` x == 0
 
-isMultipleOf3 = isMultipleOf 3
-isMultipleOf5 = isMultipleOf 5
+-- Uses an applicative functor.
+-- Map `isMultipleOf` over 3 and 5 and apply to x (as a list, because the
+-- list is the applicative functor.  This results in a list of Bool (the
+-- result from `isMultipleOf`), on which `or` is then called, because we want
+-- multiples of 3 or of 5.
+isMultiple :: Integral a => a -> Bool
+isMultiple x = or $ isMultipleOf <$> [3, 5] <*> [x]
 
-isMultiple :: Integer -> Bool
-isMultiple x = isMultipleOf3 x || isMultipleOf5 x
-
-sumMultiples :: [Integer] -> Integer
+sumMultiples :: Integral a => [a] -> a
 sumMultiples xs = sum (filter isMultiple xs)
 
 main = do
