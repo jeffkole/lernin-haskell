5b6086a598e6c2341519d8fed08c97a9853e55bc
Jeff Kolesky <jeff@kolesky.com>
2013-01-03 20:11:50 -0800

    Adds a faster `isPrime` method

--- project_euler/003_largest_prime_factor.hs
+++ project_euler/003_largest_prime_factor.hs
@@ -18,6 +18,11 @@ primeFactors :: Integral a => a -> [a]
 primeFactors x = filter isPrime $ factors x
     where isPrime x = factors x == [1, x]
 
+-- A faster way to check for prime without generating all factors.
+-- If any number from 2 up to the sqrt of the number is a factor, then
+-- n is not prime.
+isPrime n = not $ or $ [ True | x <- [2..(floor $ sqrt $ fromIntegral n)], n `mod` x == 0]
+
 main = do
     (x:xs) <- getArgs
     putStrLn $ "Largest prime factor of " ++ (show x) ++ " is " ++ (show $ maximum $ primeFactors $ read x)
